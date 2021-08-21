dede6602fd6980987be24548521d1d31c7ede850
Kipkorir2017 <pkobenj41@gmail.com>
2021-08-21 21:00:51 +0300

    add changes to quoteDetailComponent

--- Documents/CoreIPs/Quotes/src/app/quote-detail/quote-detail.component.ts
+++ Documents/CoreIPs/Quotes/src/app/quote-detail/quote-detail.component.ts
@@ -12,6 +12,12 @@ export class QuoteDetailComponent implements OnInit {
   deleteQuote(read: boolean) {
     this.isDeleted.emit(read);
   }
+  upCount(index: { upvote: number; }) {
+    index.upvote += 1;  
+  }
+  downCount(index: { downvote: number; }) {
+    index.downvote += 1;
+  }
  
   constructor() { }
 
