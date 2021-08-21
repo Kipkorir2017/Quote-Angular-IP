d133f6d1e1087bde27d40db69e7e2a261f437895
Kipkorir2017 <pkobenj41@gmail.com>
2021-08-21 08:42:39 +0300

    add constructor i the class commit and also create new quotes on the quoteComponentts

--- Documents/CoreIPs/Quotes/src/app/quote.ts
+++ Documents/CoreIPs/Quotes/src/app/quote.ts
@@ -1,2 +1,11 @@
 export class Quote {
+    toggleDetails:boolean;
+    constructor(public quote:string,
+                public author:string,
+                public submittedBy:string,
+                public upvote:number,
+                public downvote:number,
+                public datePosted:Date){
+                    this.toggleDetails=false;
+                }
 }
--- Documents/CoreIPs/Quotes/src/app/quote/quote.component.ts
+++ Documents/CoreIPs/Quotes/src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-  title = 'Quotes App';
+  title = 'Best Quotes';
+  quotes:Quote[]=[
+    new Quote("We write to taste life twice, in the moment and in retrospect","Anaïs Nin","Benjamin", 0,0, new Date(2000,5,14)),
+    new Quote("One day I will find the right words, and they will be simple","Jack Kerouac","Vitalis Kirui", 0,0, new Date(2020,2,20)),
+    new Quote("Either write something worth reading or do something worth writing","Benjamin Franklin","Teresia", 0,0, new Date(2019,3,18)),
+    new Quote("Let me live, love, and say it well in good sentences","Sylvia Plath","Joan", 0,0, new Date(2021,1,24))
+    
+  ];
 
   constructor() { }
 
