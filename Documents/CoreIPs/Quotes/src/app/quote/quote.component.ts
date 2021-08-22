92d83747bd74abed786553201f0a097cd3a67cdd
Kipkorir2017 <pkobenj41@gmail.com>
2021-08-22 13:28:28 +0300

    add changes to the quotecomponent file and also add License file

--- Documents/CoreIPs/Quotes/src/app/quote/quote.component.ts
+++ Documents/CoreIPs/Quotes/src/app/quote/quote.component.ts
@@ -12,7 +12,6 @@ export class QuoteComponent implements OnInit {
     new Quote("We write to taste life twice, in the moment and in retrospect","Anaïs Nin","Benjamin",0,0,  new Date(2000,5,14)),
     new Quote("One day I will find the right words, and they will be simple","Jack Kerouac","Vitalis Kirui",0,0, new Date(2020,2,20)),
     new Quote("Either write something worth reading or do something worth writing","Benjamin Franklin","Teresia",0,0, new Date(2019,3,18)),
-    new Quote("Let me live, love, and say it well in good sentences","Sylvia Plath","Joan",0,0,  new Date(2021,1,24))
     
   ];
 
